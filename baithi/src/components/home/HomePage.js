1536e4fd336ef79409edd2169c5f25639eec8cdb
Tien Thanh <tienthanh110202@gmail.com>
2024-07-03 21:43:53 +0700

    update ui

--- baithi/src/components/home/HomePage.js
+++ baithi/src/components/home/HomePage.js
@@ -41,12 +41,6 @@ function HoemPage() {
           </div>
         </div>
       </div>
-      <Form.Select aria-label="Default select example">
-        <option>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </Form.Select>
     </>
   );
 }
